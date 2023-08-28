8c5bf0908935fb53b118e557cff1a45789707bf9
Carl Colburn <carlcolburn@gmail.com>
2023-08-27 17:37:14 -0700

    Final commit 3

--- EmployeeSQL/sql-challenge_DB_Schema.sql
+++ EmployeeSQL/sql-challenge_DB_Schema.sql
@@ -17,13 +17,12 @@ CREATE TABLE titles(
 
 CREATE TABLE employees(
 	emp_no VARCHAR(10) NOT null PRIMARY KEY, 
-	emp_title_id VARCHAR(5) NOT NULL, 
+	emp_title_id VARCHAR(5) NOT NULL REFERENCES titles(title_id), 
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(50) NOT NULL, 
 	last_name VARCHAR(50) NOT NULL,
 	sex CHAR(1) NOT NULL, 
-	hire_date DATE NOT NULL, 
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+	hire_date DATE NOT NULL
 );
 
 CREATE TABLE departments(
@@ -32,24 +31,19 @@ CREATE TABLE departments(
 );
 
 CREATE TABLE dept_emp (
-	emp_no VARCHAR(10) NOT NULL, 
-	dept_no VARCHAR(5) NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	emp_no VARCHAR(10) NOT NULL REFERENCES employees(emp_no), 
+	dept_no VARCHAR(5) NOT NULL REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(5) NOT NULL PRIMARY KEY, 
-	emp_no VARCHAR(10) NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no), 
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no VARCHAR(10) NOT NULL REFERENCES employees(emp_no)
 );
 
 
 
 CREATE TABLE salaries(
-	emp_no INT PRIMARY KEY,
-	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no VARCHAR(10) PRIMARY KEY,
+	salary INT NOT NULL
 );
 
